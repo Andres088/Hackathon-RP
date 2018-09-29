53d9822928edaa4286e660dd841304d7849769f7
Andres Ignacio Diaz Medina <1520557@utp.edu.pe>
2018-09-29 11:06:42 -0500

    Se agregó la logica de Mall y se añadio Rubro

--- SolucionWeb/SolucionWeb/Controllers/HomeController.cs
+++ SolucionWeb/SolucionWeb/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RestSharp;
+using RestSharp.Deserializers;
 using SolucionWeb.Models;
 
 namespace SolucionWeb.Controllers
@@ -29,6 +31,12 @@ namespace SolucionWeb.Controllers
             return View();
         }
 
+        public ActionResult Rubro(string pass_mall)
+        {
+            ViewBag.mall = pass_mall;
+            return View();
+        }
+
         public ActionResult Mall(string departamento)
         {
             ViewBag.departamento = departamento;
@@ -36,6 +44,31 @@ namespace SolucionWeb.Controllers
             consulta.Add(departamento);
             Session["Consulta"] = consulta;
 
+            // Consulta API de Departamentos y genera Lista
+            RestSharp.Deserializers.JsonDeserializer deserial = new JsonDeserializer();
+            var client = new RestClient("https://api.devrealplazaonline.com/v1/departments");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Postman-Token", "50ce8858-368a-4c32-80b1-a819b4197b93");
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("x-api-key", "TDy86NqDhGkZcdbkGeJ45sFL55o69954KjVIaU6h");
+            IRestResponse response = client.Execute(request);
+            var lista = deserial.Deserialize<List<Departamento>>(response);
+            Dictionary<string, string> dep_to_code = new Dictionary<string, string>();
+            foreach (Departamento departa in lista) dep_to_code.Add(departa.depa_c_vnomb, departa.depa_c_ccod);
+
+            // Consulta API de Busqueda de Mall por Departamento
+            string codigo_depa = dep_to_code[departamento];
+            var client2 = new RestClient("https://api.devrealplazaonline.com/v1/real-estate?ps_depa_c_ccod=15");
+            var request2 = new RestRequest(Method.GET);
+            request2.AddHeader("Postman-Token", "b08fa9af-9f4d-429f-a2d2-e84557223e29");
+            request2.AddHeader("Cache-Control", "no-cache");
+            request2.AddHeader("x-api-key", "TDy86NqDhGkZcdbkGeJ45sFL55o69954KjVIaU6h");
+            IRestResponse response2 = client2.Execute(request2);
+            var lista2 = deserial.Deserialize<List<Mall>>(response2);
+            List<string> lista_malls = new List<string>();
+            foreach (Mall mimall in lista2) lista_malls.Add(mimall.inm_c_vnomb);
+
+            ViewBag.lista_malls = lista_malls;
             return View();
         }
 
@@ -139,5 +172,19 @@ namespace SolucionWeb.Controllers
 
         }
 
+    }
+
+    public class Departamento
+    {
+        public string depa_c_ccod { get; set; }
+        public string depa_c_vnomb { get; set; }
+    }
+
+    public class Mall
+    {
+        public string inm_c_icod { get; set; }
+        public string inm_c_vnomb { get; set; }
+        //public float inm_c_vlatitud { get; set; }
+        //public float inm_c_vlongitud { get; set; }
     }
 }
